fe008ccbe729be28c02c52daeab29eddaffe8d17
Keri Deemer <kerid18@att.net>
2019-09-04 23:59:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,5 +125,6 @@ def adding_to_dennis
         }
      }
      
-    programmer_hash[:dennis_ritchie][:languages][1] = 
+    programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+    p programmer_hash
 end
